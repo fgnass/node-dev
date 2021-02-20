89891c50f2f3e14c3d0d93b95e1092bca808a5b5
Bjorn Stromberg <bjorn@bjornstar.com>
2021-02-20 17:04:15 +0900

    Remove disconnect, pause watching while child restarts

--- test/spawn.js
+++ test/spawn.js
@@ -164,8 +164,10 @@ tap.test('should send IPC message when configured', t => {
   spawn('--graceful_ipc=node-dev:restart ipc-server.js', out => {
     if (out.match(/touch message.js/)) {
       touchFile('message.js');
+      let shuttingDown = false;
       return out2 => {
-        if (out2.match(/IPC received/)) {
+        if (out2.match(/IPC received/) && !shuttingDown) {
+          shuttingDown = true;
           return { exit: t.end.bind(t) };
         }
       };
