3799bf49d0b5d304a8431a7926c9c3902f2f8b06
Bjorn Stromberg <bjorn@bjornstar.com>
2021-02-22 12:21:25 +0900

    Add guard when restarting cluster

--- test/spawn.js
+++ test/spawn.js
@@ -119,14 +119,16 @@ tap.test('should relay stdin', t => {
 
 tap.test('Restart the cluster', t => {
   spawn('cluster.js', out => {
-    if (out.match(/touch message\.js/)) {
+    if (out.match(/touch message\.js/m)) {
       touchFile('message.js');
       return out2 => {
-        if (out2.match(/Restarting/)) {
+        if (out2.match(/Restarting/m)) {
           return out3 => {
-            if (out3.match(/All workers disconnected/)) {
+            if (out3.match(/All workers disconnected/m)) {
+              let shuttingDown = false;
               return out4 => {
-                if (out4.match(/touch message\.js/)) {
+                if (out4.match(/touch message\.js/) && !shuttingDown) {
+                  shuttingDown = true;
                   return { exit: t.end.bind(t) };
                 }
               };
