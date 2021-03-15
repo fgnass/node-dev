5d6e7c76ac94b18557ff40509bdb8f64a7929c47
Bjorn Stromberg <bjorn@bjornstar.com>
2021-03-15 13:44:21 +0900

    prefer arrow callbacks, and eqeqeq

--- test/log.js
+++ test/log.js
@@ -5,31 +5,31 @@ const logFactory = require('../lib/log');
 
 const noColorCfg = { ...defaultConfig, noColor: true };
 
-tap.test('log.info', function (t) {
+tap.test('log.info', t => {
   const log = logFactory(noColorCfg);
   t.like(log.info('hello'), /\[INFO\] \d{2}:\d{2}:\d{2} hello/);
   t.done();
 });
 
-tap.test('log.warn', function (t) {
+tap.test('log.warn', t => {
   const log = logFactory(noColorCfg);
   t.like(log.warn('a warning'), /\[WARN\] \d{2}:\d{2}:\d{2} a warning/);
   t.done();
 });
 
-tap.test('log.error', function (t) {
+tap.test('log.error', t => {
   const log = logFactory(noColorCfg);
   t.like(log.error('an error'), /\[ERROR\] \d{2}:\d{2}:\d{2} an error/);
   t.done();
 });
 
-tap.test('Disable the timestamp', function (t) {
+tap.test('Disable the timestamp', t => {
   const log = logFactory({ ...noColorCfg, timestamp: false });
   t.like(log.info('no timestamp'), /\[INFO\] no timestamp/);
   t.done();
 });
 
-tap.test('Custom timestamp', function (t) {
+tap.test('Custom timestamp', t => {
   const log = logFactory({ ...noColorCfg, timestamp: 'yyyy-mm-dd HH:MM:ss' });
   t.like(log.error('an error'), /\[ERROR\] \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} an error/);
   t.done();
