ef01e9170daa569584ff923d42329db66e8b524e
wclr <alex.oshchepkov@gmail.com>
2021-04-11 17:38:43 +0500

    restore deleted test

--- test/spawn/index.js
+++ test/spawn/index.js
@@ -71,6 +71,19 @@ tap.test('should restart the server with --content twice', t => {
   });
 });
 
+tap.test('should handle errors', t => {
+  spawn('error.js', out => {
+    if (out.match(/ERROR/)) {
+      touchFile('message.js');
+      return out2 => {
+        if (out2.match(/Restarting/)) {
+          return { exit: t.end.bind(t) };
+        }
+      };
+    }
+  });
+});
+
 tap.test('should handle null errors', t => {
   spawn('error-null.js', out => {
     if (out.match(/ERROR/)) {
