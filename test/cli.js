cd091f69543c969dcaf949c2052fcf03d80fe08b
Bjorn Stromberg <bjorn@bjornstar.com>
2021-05-04 11:20:08 +0900

    Improve command line argument parsing (Fixes #258)

--- test/cli.js
+++ test/cli.js
@@ -68,19 +68,19 @@ tap.test('cli overrides .node-dev.json from false to true', t => {
 tap.test('-r ts-node/register --inspect test/fixture/server.js', t => {
   const argv = 'node bin/node-dev -r ts-node/register --inspect test/fixture/server.js'.split(' ');
   const { nodeArgs } = cli(argv);
-  t.same(nodeArgs, ['-r', 'ts-node/register', '--inspect']);
+  t.same(nodeArgs, ['--inspect', '--require=ts-node/register']);
   t.end();
 });
 
 tap.test('--inspect -r ts-node/register test/fixture/server.js', t => {
   const argv = 'node bin/node-dev --inspect -r ts-node/register test/fixture/server.js'.split(' ');
   const { nodeArgs } = cli(argv);
-  t.same(nodeArgs, ['--inspect', '-r', 'ts-node/register']);
+  t.same(nodeArgs, ['--inspect', '--require=ts-node/register']);
   t.end();
 });
 
 tap.test('--expose_gc gc.js foo', t => {
-  const argv = 'node bin/node-dev --expose_gc test/fixture/gc.js test/fixture/foo'.split(' ');
+  const argv = 'node bin/node-dev --expose_gc test/fixture/gc.js foo'.split(' ');
   const { nodeArgs } = cli(argv);
   t.same(nodeArgs, ['--expose_gc']);
   t.end();
@@ -137,5 +137,149 @@ tap.test('--debounce=2000', t => {
   } = cli(['node', 'bin/node-dev', '--debounce=2000', 'test']);
 
   t.equal(debounce, 2000);
+  t.end();
+});
+
+tap.test('--require source-map-support/register', t => {
+  const { nodeArgs } = cli([
+    'node',
+    'bin/node-dev',
+    '--require',
+    'source-map-support/register',
+    'test'
+  ]);
+
+  t.same(nodeArgs, ['--require=source-map-support/register']);
+  t.end();
+});
+
+tap.test('--require=source-map-support/register', t => {
+  const { nodeArgs } = cli([
+    'node',
+    'bin/node-dev',
+    '--require=source-map-support/register',
+    'test'
+  ]);
+
+  t.same(nodeArgs, ['--require=source-map-support/register']);
+  t.end();
+});
+
+tap.test('-r source-map-support/register', t => {
+  const { nodeArgs } = cli(['node', 'bin/node-dev', '-r', 'source-map-support/register', 'test']);
+
+  t.same(nodeArgs, ['--require=source-map-support/register']);
+  t.end();
+});
+
+tap.test('-r=source-map-support/register', t => {
+  const { nodeArgs } = cli(['node', 'bin/node-dev', '-r=source-map-support/register', 'test']);
+
+  t.same(nodeArgs, ['--require=source-map-support/register']);
+  t.end();
+});
+
+tap.test('--inspect=127.0.0.1:12345', t => {
+  const { nodeArgs } = cli(['node', 'bin/node-dev', '--inspect=127.0.0.1:12345', 'test']);
+
+  t.same(nodeArgs, ['--inspect=127.0.0.1:12345']);
+  t.end();
+});
+
+tap.test('--inspect', t => {
+  const { nodeArgs } = cli(['node', 'bin/node-dev', '--inspect', 'test']);
+
+  t.same(nodeArgs, ['--inspect']);
+  t.end();
+});
+
+tap.test('--require source-map-support/register --require ts-node/register', t => {
+  const { nodeArgs } = cli([
+    'node',
+    'bin/node-dev',
+    '--require',
+    'source-map-support/register',
+    '--require',
+    'ts-node/register',
+    'test'
+  ]);
+
+  t.same(nodeArgs, ['--require=source-map-support/register', '--require=ts-node/register']);
+  t.end();
+});
+
+// This should display usage information at some point
+tap.test('No script or option should fail', t => {
+  t.throws(() => cli(['node', 'bin/node-dev']));
+  t.end();
+});
+
+tap.test('Just an option should fail', t => {
+  t.throws(() => cli(['node', 'bin/node-dev', '--option']));
+  t.end();
+});
+
+tap.test('Just an option with a value should fail', t => {
+  t.throws(() => cli(['node', 'bin/node-dev', '--option=value']));
+  t.end();
+});
+
+tap.test('An unknown argument with a value instead of a script should fail.', t => {
+  t.throws(() => cli(['node', 'bin/node-dev', '--unknown-arg', 'value']));
+  t.end();
+});
+
+tap.test('An unknown argument with a value', t => {
+  const { nodeArgs } = cli(['node', 'bin/node-dev', '--unknown-arg=value', 'test']);
+
+  t.same(nodeArgs, ['--unknown-arg=value']);
+  t.end();
+});
+
+tap.test('An unknown argument without a value can use -- to delimit', t => {
+  // use -- to delimit the end of options
+  const { nodeArgs } = cli(['node', 'bin/node-dev', '--unknown-arg', '--', 'test']);
+
+  t.same(nodeArgs, ['--unknown-arg']);
+  t.end();
+});
+
+tap.test('Single dash with value', t => {
+  const { nodeArgs } = cli(['node', 'bin/node-dev', '-u', 'value', 'test']);
+
+  t.same(nodeArgs, ['-u=value']);
+  t.end();
+});
+
+tap.test('Single dash with = and value', t => {
+  const { nodeArgs } = cli(['node', 'bin/node-dev', '-u=value', 'test']);
+
+  t.same(nodeArgs, ['-u=value']);
+  t.end();
+});
+
+tap.test('Single dash without value should fail', t => {
+  t.throws(() => cli(['node', 'bin/node-dev', '-u', 'test']));
+  t.end();
+});
+
+tap.test('Single dash without value can use -- to delimit', t => {
+  const { nodeArgs } = cli(['node', 'bin/node-dev', '-u', '--', 'test']);
+
+  t.same(nodeArgs, ['-u']);
+  t.end();
+});
+
+tap.test('Repeated single dash', t => {
+  const { nodeArgs } = cli(['node', 'bin/node-dev', '-u=value1', '-u=value2', 'test']);
+
+  t.same(nodeArgs, ['-u=value1', '-u=value2']);
+  t.end();
+});
+
+tap.test('Repeated single dash without =', t => {
+  const { nodeArgs } = cli(['node', 'bin/node-dev', '-u', 'value1', '-u', 'value2', 'test']);
+
+  t.same(nodeArgs, ['-u=value1', '-u=value2']);
   t.end();
 });
