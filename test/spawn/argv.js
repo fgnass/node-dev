b5c8b10e97666a3f7fb6f7098d0e96e765d20496
Kyle Herock <kherock@gmail.com>
2022-03-26 00:19:01 -0400

    [ESM] always load the ESM loader

--- test/spawn/argv.js
+++ test/spawn/argv.js
@@ -6,7 +6,7 @@ tap.test('should not show up in argv', t => {
   spawn('argv.js foo', out => {
     const argv = JSON.parse(out.replace(/'/g, '"'));
     t.match(argv[0], /.*?node(js|\.exe)?$/);
-    t.equal(argv[1], 'argv.js');
+    t.match(argv[1], /.*[\\/]argv\.js$/);
     t.equal(argv[2], 'foo');
     return { exit: t.end.bind(t) };
   });
